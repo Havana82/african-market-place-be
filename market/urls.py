ce661b8c6ceedf93192492d51c3e8ef1fb763586
halimAremu <halimat3000@gmail.com>
2023-08-01 17:06:16 -0500

    minor fix

--- market/urls.py
+++ market/urls.py
@@ -8,6 +8,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('market_api.urls')),
-] + static(settings.MEDIA_URL, documents_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
